63b02358dc83ec42b717ddd248c4e3f41ad6ee87
Daniela <danisalazarcueto7@gmail.com>
2022-04-07 17:35:45 +0100

    add sass for styles

---
+++ js/00main.js
@@ -0,0 +1,8 @@
+/*- Select and submit a number rating
+- See the "Thank you" card state after submitting a rating*/
+'use strict'; 
+
+const item = document.querySelector('.item-js'); 
+const button = document.querySelector('.button')
+
+
