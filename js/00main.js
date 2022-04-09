2a13b5bdfc49542d54b6945af95e347b9081ccc3
Daniela <danisalazarcueto7@gmail.com>
2022-04-09 14:36:41 +0100

    add function js with form info

--- js/00main.js
+++ js/00main.js
@@ -2,7 +2,30 @@
 - See the "Thank you" card state after submitting a rating*/
 'use strict'; 
 
-const item = document.querySelector('.item-js'); 
-const button = document.querySelector('.button')
+
+const container = document.querySelector('.main')
+const formulary = document.querySelector('form')
+
+function handleSubmit(ev){
+    ev.preventDefault() 
+    const formData = new FormData (formulary)
+    console.log(formData);
+    const selectedRating = formData.get('select'); 
+    console.log(selectedRating);
+    container.innerHTML = 
+    `<div class="main"><img src ="./images/illustration-thank-you.svg" alt="thank you" >
+    <p>You selected ${selectedRating} out of 5</p>
+    <h2>Thank you!</h2>
+    <p>We apreciate you taking time to give a rating. If you ever need more support, don't hesitate to get in touch</p>
+    </div>`
+
+}
+
+
+
+
+formulary.addEventListener('submit', handleSubmit)
+
+
 
 
